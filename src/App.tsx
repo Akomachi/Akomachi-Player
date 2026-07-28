7cc93b0c97727cf78ba7b611144c1ac031de7572
Ako <61356175+55tr23@users.noreply.github.com>
2026-07-28 09:55:56 -0700

    Updated position of button and text

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ export default function App() {
         void handleFiles(e.dataTransfer.files)
       }}
     >
-      <header className="flex items-center gap-5 border-b border-line px-6 py-4">
+      <header className="flex items-center border-b border-line px-6 py-4">
         <h1 className="text-5xl font-medium pl-3 pr-6 text-heading">Library</h1>
         <button
           onClick={() => fileInput.current?.click()}
@@ -47,7 +47,7 @@ export default function App() {
         >
           Add files
         </button>
-        <h1 className="text-3xl text-heading absolute right-35 top-6">Akomachi</h1>
+        <h1 className="text-3xl text-heading absolute right-32 top-6">Akomachi</h1>
         <img className="h-28 w-28 object-scale-down absolute right-3 top-2" src="src/assets/icon.png"></img>
       </header>
 
